cb9745f1a4570e90324d0d0c0f823c6001499eb3
telumo <drumscohika@gmail.com>
2020-05-25 21:17:17 +0900

    update giflet.rb

--- Formula/giflet.rb
+++ Formula/giflet.rb
@@ -1,9 +1,9 @@
 class Giflet < Formula
   desc "cli gif tool in Rust"
   homepage "https://github.com/telumo/giflet"
-  url "https://github.com/telumo/giflet/releases/download/v0.1.0/giflet-0.1.0-x86_64-apple-darwin.tar.gz"
-  sha256 "a5ae76eb7927e9b7e04f730421e51220cae29b363ec605604531cb77fca2ae6e"
-  version "0.1.0"
+  url "https://github.com/telumo/giflet/releases/download/v0.1.1-alpha/giflet-0.1.1-x86_64-apple-darwin.tar.gz"
+  sha256 "241e576becf5e06b064dac1e61920f37d5a05876623fb87b9f4bf16c9bdab39d"
+  version "0.1.1"
 
   def install
     bin.install "giflet"
